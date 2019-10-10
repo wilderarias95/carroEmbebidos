22fb5a986a321a8007d27a65acb3b5dd95b22776
Wilder Arias <wilder.arias95@gmail.com>
2019-10-10 15:32:25 -0500

    librería con los requerimientos del parcial

--- source/PWMControl.c
+++ source/PWMControl.c
@@ -9,21 +9,24 @@
 #include "clock_config.h"
 #include "MKL27Z644.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
-
-/* TODO: insert other definitions and declarations here. */
 
 /*
  Backward [1160, 1600]
  Forward  [1120,600]
  stop 1125
  */
-#define SetDutyTPM1CH0(x) TPM1->CONTROLS[0].CnV=(uint32_t)(x)
 volatile int timeOver = 1000;
 volatile int delayValue = 250000;
-volatile static int STOP_ENGINE = 1125;
+volatile static int STOP_ENGINE = 1125; //valor PWM para STOP
+volatile float maxFwSpeed; //velocidad máxima hacía adelante
+volatile float minFwSpeed; //velocidad mínima hacía adelante
+volatile float maxBwSpeed; //velocidad máxima hacía atrás
+volatile float minBwSpeed; //velocidad mínima hacía atrás
+volatile float wheelR; //diametro de la rueda
+volatile int motorKV; //Número de RPM que puede alcanzar el motor
 
-void PWMInit(void){
+void InitDriver(int MotorKV, float WheelR, float MaxFwSpeed, float MinFwSpeed, float MaxBwSpeed, float MinBwSpeed){
+	/****** Inicializa el TPM ******/
 	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK; //TPM1 clock gate control enable
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); //tpm clock source 0b01 IRC48M clock
 
@@ -43,6 +46,14 @@ void PWMInit(void){
 	//TPM1->CONTROLS[0].CnSC |= TPM_CnSC_CHIE_MASK; //Channel Interrupt Enable
 
 	TPM1->SC |= TPM_SC_CMOD(1); //CLOCK MODE SELECTION 0b01 TPM counter increments on every TPM counter clock
+
+	/***** inicializar parametros carro *****/
+	motorKV = MotorKV;
+	wheelR = WheelR;
+	maxFwSpeed = MaxFwSpeed;
+	minFwSpeed = MinFwSpeed;
+	maxBwSpeed = MaxBwSpeed;
+	minBwSpeed = MinBwSpeed;
 }
 
 void SetSpeed(int delay, float speed){
@@ -50,33 +61,37 @@ void SetSpeed(int delay, float speed){
 	while(delay)
 	{
 		delay--;
-		//printf("\ndelay %d=",delay);
 	}
-	//printf("Aqui tambien entro");
 }
 
-void SetBackwardSpeed(float Speed){
-	//printf("\nEntro a la funcion\n");
-	SetSpeed(delayValue, STOP_ENGINE);
-	SetSpeed(delayValue, 1300);
-	SetSpeed(delayValue, 1500);
-	SetSpeed(delayValue, 1300);
-	SetSpeed(delayValue, STOP_ENGINE);
-	SetSpeed(delayValue, Speed);
+int SetBackwardSpeed(float Speed){ //Backward [1160, 1600]
+	if(Speed >= minBwSpeed && Speed <= maxBwSpeed){
+		SetSpeed(delayValue, STOP_ENGINE);
+		SetSpeed(delayValue, 1300);
+		SetSpeed(delayValue, 1500);
+		SetSpeed(delayValue, 1300);
+		SetSpeed(delayValue, STOP_ENGINE);
+		SetSpeed(delayValue, Speed);
+		return 0;
+	}else{
+		return -1;
+	}
 }
 
-void SetFordwardSpeed(float Speed){
-	SetSpeed(1000, Speed);
+int SetFordwardSpeed(float Speed){ //Forward  [1120,600]
+	if(Speed >= maxFwSpeed && Speed <= minFwSpeed){
+		SetSpeed(1000, Speed);
+		return 0;
+	}else{
+		return -1;
+	}
 }
 
-void Stop(void){
+void Stop(void){// stop 1125
 	SetSpeed(1000, STOP_ENGINE);
 }
 
-
-
 int main(void) {
-
 	//--------------Default Configuration ----------------------//
   	/* Init board hardware. */
     BOARD_InitBootPins();
@@ -86,7 +101,7 @@ int main(void) {
     BOARD_InitDebugConsole();
 
     //Programmer configuration
-    PWMInit();
+    InitDriver( 0, 0, 600, 1120, 1600, 1160);
 
     PRINTF("PWM Control\n");
     /* Force the counter to be placed into memory. */
@@ -105,10 +120,10 @@ int main(void) {
     SetBackwardSpeed(sp);
 
     timeOver2 = timeOver*20000;
-      while(timeOver2){
-      	timeOver2--;
-      }
-     Stop();
+    while(timeOver2){
+      timeOver2--;
+    }
+    Stop();
     while(1) {
         i++ ;
     }
